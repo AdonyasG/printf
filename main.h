681a9da04109b2df9bca6a9e18a90324dd01bbdf
AdonyasG <adonyas2000@gmail.com>
2022-07-14 13:54:51 -0700

    header file

---
+++ main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int print_int(va_list args);
+int print_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int print_percent(__attribute__((unused)) va_list args);
+int print_char(va_list val);
+int print_string(va_list val);
+int finder(const char *format, convert_match m[], va_list args);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
