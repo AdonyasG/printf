5b18a57c885aba88e9e93aed12aae6d4e404de8f
AdonyasG <adonyas2000@gmail.com>
2022-07-15 00:33:56 -0700

    percent added

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_percent - prints percent
+ * @args: number of argument
+ * Return: 1
+ */
+
+int print_percent(__attribute__((unused)) va_list args)
+{
+	_putchar('%');
+	return (1);
+}
